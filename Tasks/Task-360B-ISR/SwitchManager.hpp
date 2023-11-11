8c95bc2080235f16a37ececd5adb18c9d8fea85d
RhynJ <87622045+RhynJ@users.noreply.github.com>
2023-11-11 13:40:23 +0000

    threads

--- Tasks/Task-360B-ISR/SwitchManager.hpp
+++ Tasks/Task-360B-ISR/SwitchManager.hpp
@@ -1,5 +1,11 @@
 #ifndef __SWITCH_MANAGER__
 #define __SWITCH_MANAGER__
+#include "uop_msb.h"
+
+#include <chrono>
+#include <ratio>
+using namespace uop_msb;
+using namespace chrono;
 
 #include "mbed.h"
 // This class manages an Interrupt in and LED output
@@ -21,6 +27,7 @@ public:
       : switchInterrupt(intInPin), led(gpioPin) {
     count = 0;
     // Listen for rising edge
+    //this is almost like a definition as to what to look out for
     switchInterrupt.rise(callback(this, &SwitchManager::waitForRising));
   }
   ~SwitchManager() {
@@ -32,7 +39,74 @@ public:
   }
 
   //This is a STATIC member of the CLASS
-  static uint32_t getCount() { return count; }
+  
+  static uint32_t getCount(){
+    //this ensures that nothing can interrupt this while it is running
+    CriticalSectionLock lock;
+    return count; 
+  }
+};
+
+class Flashy {
+    private:
+    //Composition in action here
+    DigitalOut led;
+    Ticker tick;
+    //Internal state
+    microseconds _interval;
+    PinName _pin;
+    bool _enabled = false;
+
+    void timerISR()  {
+        led = !led;
+    }
+
+    void enable(bool en)
+    {
+        if (en == _enabled) return;
+
+        if (en) {
+            tick.attach(callback(this, &Flashy::timerISR), _interval);
+        } else {
+            tick.detach();
+            led = 0;
+        }
+        _enabled = en;
+    }
+
+    public:
+    Flashy(PinName pin, microseconds t) : led(pin,0), _pin(pin), _interval(t) {
+        enable(false);
+    }
+    //OVERLOAD constructor
+    Flashy(PinName pin) : Flashy(pin, 500ms) {
+    }
+    ~Flashy() {
+        enable(false);
+        led = 0;
+    }
+
+    //OVERLOAD private enable
+    void enable() {
+        enable(true);
+    }
+
+    void disable() {
+        enable(false);
+    }
+
+    //Setter and getter for _inteval
+    void setInterval(microseconds t) {
+        _interval = t;
+        if (_enabled) {
+            //Reenable to force update
+            disable();
+            enable();
+        }
+    }
+    microseconds getInterval() {
+        return _interval;
+    }
 };
 
 #endif
--- Tasks/Task-360B-ISR/main.cpp
+++ Tasks/Task-360B-ISR/main.cpp
@@ -6,28 +6,34 @@
 #define TRAF_RED1_PIN PC_2
 #define BTN1_PIN PG_0
 #define BTN2_PIN PG_1
+#define USER_LED1 PB_0  // this is the green user LED 
 
-Ticker yellowTick;
-DigitalOut  yellow_led(TRAF_YEL1_PIN);
 
-void toggleYellowISR() {
-    yellow_led = !yellow_led;    
-}
+
+
     
 int main() {
     
+    Flashy FlashUserGreen(USER_LED1, 500ms);
+    FlashUserGreen.enable();
+
+    Flashy FlashTRAFYellow(TRAF_YEL1_PIN, 500ms);
+    FlashTRAFYellow.enable();
+
+
+
     //Interrupt controlled red led using BTN1
     SwitchManager sm1(BTN1_PIN, TRAF_RED1_PIN);
 
     //Interrupt controlled green led using BTN2    
     SwitchManager sm2(BTN2_PIN, TRAF_GRN1_PIN);    
     
-    //Simple ticker for the yellow LED
-    yellowTick.attach(&toggleYellowISR, 500ms);
+
     
     //Now loop forever
     while(1) { 
         sleep();
+        //this is static so only belong to the class, cannot be changed outside the class
         printf("count=%u\n",SwitchManager::getCount());
     };
 }
